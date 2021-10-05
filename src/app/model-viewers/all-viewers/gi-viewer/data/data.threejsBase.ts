a9993e5d7628110df96887acf5df96d0873ecbdd
phuongtung1 <phuongtung1231992@yahoo.com>
2021-10-05 16:42:05 +0800

    clearer lines

--- src/app/model-viewers/all-viewers/gi-viewer/data/data.threejsBase.ts
+++ src/app/model-viewers/all-viewers/gi-viewer/data/data.threejsBase.ts
@@ -132,7 +132,7 @@ export class DataThreejsBase {
         this.renderer = new THREE.WebGLRenderer({
             antialias: true,
             alpha: true,
-            logarithmicDepthBuffer: true,
+            // logarithmicDepthBuffer: true,
             powerPreference: 'high-performance'
         });
         this.renderer.autoClear = false;
